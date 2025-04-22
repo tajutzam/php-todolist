9a01da81e3b01d54912344e8c8a0a01a1c3e22aa
tajutzam <mohammadtajutzamzami07@gmail.com>
2025-04-22 11:44:52 +0700

    test

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$host = "172.31.12.226";  // Ganti dengan endpoint RDS
+$host = "todolistdb.cbyieygqmvuh.ap-southeast-2.rds.amazonaws.com";  // Ganti dengan endpoint RDS
 $dbname = "todolist_db";  // Nama database yang digunakan
 $username = "root";  // Username RDS kamu
 $password = "Rahsia123.";
